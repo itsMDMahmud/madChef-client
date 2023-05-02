ad93bdce86c508cdedea67c7ba76841f400dc41c
itsMDMahmud <itsmdmahmud@gmail.com>
2023-05-03 04:45:15 +0600

    details data got

--- src/components/ChefDetails/ChefDetails.jsx
+++ src/components/ChefDetails/ChefDetails.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const ChefDetails = () => {
+    const { id } = useParams();
+    // console.log(id);
+  const [chefDetails, setChefDetails] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5500/chefdata")
+          .then((res) => res.json())
+          .then((data) => {
+            if (data) {
+              setChefDetails(data);
+            }
+          });
+      }, []);
+
+    const chefData = chefDetails.find((chef) => chef.id == id);
+    // console.log(chefData);
+
     return (
         <div>
             <h2>chef details</h2>
